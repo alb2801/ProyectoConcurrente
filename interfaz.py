401da42ee927e418ab972bcd9998c4e1439cae37
alb2801 <alberthpulgarin@gmail.com>
2023-12-12 22:19:36 -0500

    Imagenes aleatorias

--- interfaz.py
+++ interfaz.py
@@ -2,6 +2,15 @@
 
 import streamlit as st
 from DescargarIma import download_images
+import os
+import random
+from PIL import Image
+
+def select_random_images():
+    image_folder = "imagenes"
+    images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
+    random_images = random.sample(images, 10)
+    return [Image.open(os.path.join(image_folder, img)) for img in random_images]
 
 def main():
     st.title("Proyecto Concurrente")
@@ -10,13 +19,14 @@ def main():
     tema = st.text_input("Ingrese el tema:")
 
     # Botón para ejecutar la descarga y duplicación de imágenes
-    if st.button("Descargar imagenes"):
+    if st.button("Descargar imágenes"):
         download_images(tema)
-        st.success("Descargas de imagenes completadas.")
+        st.success("Descargas de imágenes completadas.")
 
+    
     # Selección en lista desplegable para el tipo de paralelismo
     tipo_paralelismo = st.selectbox("Seleccione el tipo de paralelismo:",
-                                    ["C", "OpenMP", "Multiprocessing", "MPI4PY"])
+                                    ["Multiprocessing", "MPI4PY"])
 
     # Configuración según el tipo de paralelismo
     if tipo_paralelismo == "C" or tipo_paralelismo == "OpenMP":
@@ -27,21 +37,15 @@ def main():
         num_procesos = st.slider("Seleccione la cantidad de procesos:", 1, 10, 1)
 
     # Lista desplegable con 10 items
-    lista_items = st.selectbox("Seleccione un ítem de la lista:", ["Class 1","Class 2","Class 3",
+    lista_items = st.selectbox("Seleccione el flitro a aplicar:", ["Class 1","Class 2","Class 3",
                                                                    "Square 3x3","Edge 3x3","Square 5x5",
-                                                                   "Edge 5x5","Sobel vertical y horizontalmente",
-                                                                   "Laplace","Prewitt vertical y horizontal"])
+                                                                   "Edge 5x5","Sobel vertical","Sobel horizontalmente"
+                                                                   "Laplace","Prewitt vertical","Prewitt horizontal"])
 
-    # Mostrar la configuración seleccionada
-    st.text(f"Tipo de paralelismo: {tipo_paralelismo}")
-
-    if tipo_paralelismo == "C" or tipo_paralelismo == "OpenMP":
-        st.text(f"Cantidad de hilos: {num_hilos}")
-    elif tipo_paralelismo == "Multiprocessing" or tipo_paralelismo == "MPI4PY":
-        st.text(f"Cantidad de procesos: {num_procesos}")
-
-    st.text(f"Ítem seleccionado: {lista_items}")
-
+    if st.button("Mostrar 10 imágenes aleatorias"):
+        random_images = select_random_images()
+        for img in random_images:
+            st.image(img,  width=300) 
 
 if __name__ == "__main__":
     main()
