34b82de2bcc7e4f18f683e75784d37a79b13301d
alb2801 <alberthpulgarin@gmail.com>
2023-12-12 23:55:43 -0500

    Interfaz MPI4PY

--- FiltradoMPI4PY.py
+++ FiltradoMPI4PY.py
@@ -3,130 +3,133 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 
-filter_type='laplace'
-# Inicializar MPI
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
+def apply_filter(num_procesos, filter_type, random_images):
+    # Inicializar MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
 
-# Cargar la imagen desde la web (reemplaza 'URL_de_tu_imagen.jpg' con la URL de tu imagen)
-image_url = 'messi.jpg'
-image = cv2.imread(image_url)
+    # Cargar la imagen desde la web (reemplaza 'URL_de_tu_imagen.jpg' con la URL de tu imagen)
+    image_url = random_images
+    image = cv2.imread(image_url)
 
-# Dividir la imagen en partes iguales entre los procesos
-rows_per_process = image.shape[0] // size
-start_row = rank * rows_per_process
-end_row = (rank + 1) * rows_per_process if rank != size - 1 else image.shape[0]
 
-# Seleccionar la porción de la imagen para este proceso
-local_image = image[start_row:end_row, :]
+    # Dividir la imagen en partes iguales entre los procesos
+    rows_per_process = image.shape[0] // size
+    start_row = rank * rows_per_process
+    end_row = (rank + 1) * rows_per_process if rank != size - 1 else image.shape[0]
 
-#Definir el kernel
-if filter_type == 'class1':
+    # Seleccionar la porción de la imagen para este proceso
+    local_image = image[start_row:end_row, :]
+
+    #Definir el kernel
+    if filter_type == 'Class 1':
+            kernel = np.array([[0, 0, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, -1, 0, 0],
+                            [0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Class 2':
         kernel = np.array([[0, 0, 0, 0, 0],
-                           [0, 0, 1, 0, 0],
-                           [0, 0, -1, 0, 0],
-                           [0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 0]])
-elif filter_type == 'class2':
-    kernel = np.array([[0, 0, 0, 0, 0],
-                        [0, 0, 1, 0, 0],
-                        [0, 0, -2, 0, 0],
-                        [0, 0, 1, 0, 0],
-                        [0, 0, 0, 0, 0]])
-elif filter_type == 'class3':
-    kernel = np.array([[0, 0, -1, 0, 0],
-                        [0, 0, 3, 0, 0],
-                        [0, 0, -3, 0, 0],
-                        [0, 0, 1, 0, 0],
-                        [0, 0, 0, 0, 0]])
-elif filter_type == 'square3x3':
-    kernel = np.array([[0, 0, 0, 0, 0],
-                        [0, -1, 2, -1, 0],
-                        [0, 2, -4, 2, 0],
-                        [0, -1, 2, -1, 0],
-                        [0, 0, 0, 0, 0]])
-elif filter_type == 'edge3x3':
-    kernel = np.array([[0, 0, 0, 0, 0],
-                        [0, -1, 2, -1, 0],
-                        [0, 2, -4, 2, 0],
-                        [0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0]])
-elif filter_type == 'square5x5':
-    kernel = np.array([[-1, 2, -2, 2, -1],
-                        [2, -6, 8, -6, 2],
-                        [-2, 8, -12, 8, -2],
-                        [2, -6, 8, -6, 2],
-                        [-1, 2, -2, 2, -1]])
-elif filter_type == 'edge5x5':
-    kernel = np.array([[-1, 2, -2, 2, -1],
-                        [2, -6, 8, -6, 2],
-                        [-2, 8, -12, 8, -2],
-                        [0, 0, 0, 0, 0],
-                        [0, 0, 0, 0, 0]])
-elif filter_type == 'sobel_vertical':
-    kernel = np.array([[-1, 0, 1],
-                        [-2, 0, 2],
-                        [-1, 0, 1]])
-elif filter_type == 'sobel_horizontal':
-    kernel = np.array([[-1, -2, -1],
-                        [0, 0, 0],
-                        [1, 2, 1]])
-elif filter_type == 'laplace':
-    kernel = np.array([[-1, -1, -1],
-                        [-1, 8, -1],
-                        [-1, -1, -1]])
-elif filter_type == 'prewitt_v':
-    kernel = np.array([[-1, 0, 1],
-                        [-1, 0, 1],
-                        [-1, 0, 1]])
-elif filter_type == 'prewitt_h':
-    kernel = np.array([[-1, -1, -1],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, -2, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Class 3':
+        kernel = np.array([[0, 0, -1, 0, 0],
+                            [0, 0, 3, 0, 0],
+                            [0, 0, -3, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Square 3x3':
+        kernel = np.array([[0, 0, 0, 0, 0],
+                            [0, -1, 2, -1, 0],
+                            [0, 2, -4, 2, 0],
+                            [0, -1, 2, -1, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Edge 3x3':
+        kernel = np.array([[0, 0, 0, 0, 0],
+                            [0, -1, 2, -1, 0],
+                            [0, 2, -4, 2, 0],
+                            [0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Square 5x5':
+        kernel = np.array([[-1, 2, -2, 2, -1],
+                            [2, -6, 8, -6, 2],
+                            [-2, 8, -12, 8, -2],
+                            [2, -6, 8, -6, 2],
+                            [-1, 2, -2, 2, -1]])
+    elif filter_type == 'Edge 5x5':
+        kernel = np.array([[-1, 2, -2, 2, -1],
+                            [2, -6, 8, -6, 2],
+                            [-2, 8, -12, 8, -2],
+                            [0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0]])
+    elif filter_type == 'Sobel':
+        kernel = np.array([[-1, 0, 1],
+                            [-2, 0, 2],
+                            [-1, 0, 1]])
+    elif filter_type == 'sobel_horizontal':
+        kernel = np.array([[-1, -2, -1],
                             [0, 0, 0],
-                            [1, 1, 1]])
-else:
-    raise ValueError("Tipo de filtro no válido")
+                            [1, 2, 1]])
+    elif filter_type == 'Laplace':
+        kernel = np.array([[-1, -1, -1],
+                            [-1, 8, -1],
+                            [-1, -1, -1]])
+    elif filter_type == 'prewitt_v':
+        kernel = np.array([[-1, 0, 1],
+                            [-1, 0, 1],
+                            [-1, 0, 1]])
+    elif filter_type == 'Prewitt':
+        kernel = np.array([[-1, -1, -1],
+                                [0, 0, 0],
+                                [1, 1, 1]])
+    else:
+        raise ValueError("Tipo de filtro no válido")
 
-# Convertir la porción de la imagen a escala de grises
-local_gray_image = cv2.cvtColor(local_image, cv2.COLOR_BGR2GRAY)
+    # Convertir la porción de la imagen a escala de grises
+    local_gray_image = cv2.cvtColor(local_image, cv2.COLOR_BGR2GRAY)
 
-# Aplicar el filtro convolucional localmente
-local_filtered_image = cv2.filter2D(local_gray_image, -1, kernel)
+    # Aplicar el filtro convolucional localmente
+    local_filtered_image = cv2.filter2D(local_gray_image, -1, kernel)
 
-# Recopilar todas las porciones filtradas
-filtered_images = comm.gather(local_filtered_image, root=0)
+    # Recopilar todas las porciones filtradas
+    filtered_images = comm.gather(local_filtered_image, root=0)
 
-# El proceso 0 muestra las imágenes
-if rank == 0:
-    # Combinar las imágenes filtradas
-    filtered_image = np.vstack(filtered_images)
+    # El proceso 0 muestra las imágenes
+    if rank == 0:
+        # Combinar las imágenes filtradas
+        filtered_image = np.vstack(filtered_images)
 
-    # Mostrar las dimensiones de la imagen filtrada
-    print(f"Dimensiones de la imagen filtrada: {filtered_image.shape}")
+        # Mostrar las dimensiones de la imagen filtrada
+        print(f"Dimensiones de la imagen filtrada: {filtered_image.shape}")
 
-    # Obtener valor mínimo, máximo, medio y desviación estándar de los píxeles
-    min_val = np.min(filtered_image)
-    max_val = np.max(filtered_image)
-    mean_val = np.mean(filtered_image)
-    std_dev = np.std(filtered_image)
+        # Obtener valor mínimo, máximo, medio y desviación estándar de los píxeles
+        min_val = np.min(filtered_image)
+        max_val = np.max(filtered_image)
+        mean_val = np.mean(filtered_image)
+        std_dev = np.std(filtered_image)
 
-    print(f"Valor mínimo de los píxeles: {min_val}")
-    print(f"Valor máximo de los píxeles: {max_val}")
-    print(f"Valor medio de los píxeles: {mean_val}")
-    print(f"Desviación estándar de los píxeles: {std_dev}")
+        print(f"Valor mínimo de los píxeles: {min_val}")
+        print(f"Valor máximo de los píxeles: {max_val}")
+        print(f"Valor medio de los píxeles: {mean_val}")
+        print(f"Desviación estándar de los píxeles: {std_dev}")
 
-    # Mostrar la imagen original y la imagen filtrada
-    plt.figure(figsize=(10, 5))
+        # Mostrar la imagen original y la imagen filtrada
+        plt.figure(figsize=(10, 5))
 
-    plt.subplot(1, 2, 1)
-    plt.title('Imagen Original')
-    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
+        plt.subplot(1, 2, 1)
+        plt.title('Imagen Original')
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
 
-    plt.subplot(1, 2, 2)
-    plt.title('Imagen Filtrada')
-    plt.imshow(filtered_image, cmap='gray')
-    plt.axis('off')
+        plt.subplot(1, 2, 2)
+        plt.title('Imagen Filtrada')
+        plt.imshow(filtered_image, cmap='gray')
+        plt.axis('off')
 
-    plt.tight_layout()
-    plt.show()
+        plt.tight_layout()
+        plt.show()
+        
+        return filtered_image
--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,7 @@
-# interfaz.py
-
+import numpy as np
 import streamlit as st
 from DescargarIma import download_images
+from FiltradoMPI4PY import apply_filter
 import os
 import random
 from PIL import Image
@@ -10,7 +10,7 @@ def select_random_images():
     image_folder = "imagenes"
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
     random_images = random.sample(images, 10)
-    return [Image.open(os.path.join(image_folder, img)) for img in random_images]
+    return [os.path.join(image_folder, img) for img in random_images]
 
 def main():
     st.title("Proyecto Concurrente")
@@ -23,29 +23,40 @@ def main():
         download_images(tema)
         st.success("Descargas de imágenes completadas.")
 
-    
     # Selección en lista desplegable para el tipo de paralelismo
     tipo_paralelismo = st.selectbox("Seleccione el tipo de paralelismo:",
                                     ["Multiprocessing", "MPI4PY"])
 
-    # Configuración según el tipo de paralelismo
-    if tipo_paralelismo == "C" or tipo_paralelismo == "OpenMP":
-        # Cantidad de hilos (max 10)
-        num_hilos = st.slider("Seleccione la cantidad de hilos:", 1, 10, 1)
-    elif tipo_paralelismo == "Multiprocessing" or tipo_paralelismo == "MPI4PY":
+    if tipo_paralelismo == "Multiprocessing" or tipo_paralelismo == "MPI4PY":
         # Cantidad de procesos (max 10)
         num_procesos = st.slider("Seleccione la cantidad de procesos:", 1, 10, 1)
 
     # Lista desplegable con 10 items
-    lista_items = st.selectbox("Seleccione el flitro a aplicar:", ["Class 1","Class 2","Class 3",
-                                                                   "Square 3x3","Edge 3x3","Square 5x5",
-                                                                   "Edge 5x5","Sobel vertical","Sobel horizontalmente"
-                                                                   "Laplace","Prewitt vertical","Prewitt horizontal"])
+    lista_items = st.selectbox("Seleccione el filtro a aplicar:", ["Class 1", "Class 2", "Class 3",
+                                                                   "Square 3x3", "Edge 3x3", "Square 5x5",
+                                                                   "Edge 5x5", "Sobel", "Laplace", "Prewitt"])
 
-    if st.button("Mostrar 10 imágenes aleatorias"):
+    if st.button("Aplicar filtro"):
         random_images = select_random_images()
-        for img in random_images:
-            st.image(img,  width=300) 
+        for img_path in random_images:
+            img = Image.open(img_path)
+
+            # Crear dos columnas para mostrar las imágenes
+            col1, col2 = st.columns(2)
+
+            # Mostrar la imagen original a la izquierda
+            col1.image(img, caption='Imagen Original', use_column_width=True)
+
+            filtered_image = apply_filter(num_procesos, lista_items, img_path)
+
+            # Mostrar estadísticas
+            col2.write(f"Valor mínimo de los píxeles: {np.min(filtered_image)}")
+            col2.write(f"Valor máximo de los píxeles: {np.max(filtered_image)}")
+            col2.write(f"Valor medio de los píxeles: {np.mean(filtered_image)}")
+            col2.write(f"Desviación estándar de los píxeles: {np.std(filtered_image)}")
+
+            # Mostrar la imagen filtrada a la derecha
+            col2.image(filtered_image, caption='Imagen Filtrada', use_column_width=True, channels='GRAY')
 
 if __name__ == "__main__":
     main()
